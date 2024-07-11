3c063160d1d8ce3a91d9f8efff34274d71b653e8
kawaguchikohei <kou.1114.2002@docomo.ne.jp>
2024-07-11 11:22:34 +0900

    added agent images

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useLocation } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
+  const search = useLocation().search;
+  const query = new URLSearchParams(search);
+  const name = query.get('server')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <h1>React Bootstrap</h1>
+        {name ? <h2>Server: {name}</h2> : null}
+      </Container>
+    </>
   );
 }
 
